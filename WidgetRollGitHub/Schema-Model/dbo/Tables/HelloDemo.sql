2fc1086f4932d678d82afd284a2da66778550364
Andrew Pierce <andrew.pierce@red-gate.com>
2018-05-17 08:37:57 -0700

    added a column and some data

--- WidgetRollGitHub/Schema-Model/dbo/Tables/HelloDemo.sql
+++ WidgetRollGitHub/Schema-Model/dbo/Tables/HelloDemo.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[HelloDemo]
 (
 [DemoId] [bigint] NOT NULL,
-[DemoFName] [nvarchar] (25) NULL
+[DemoFName] [nvarchar] (25) NULL,
+[DemoLName] [nvarchar] (25) NULL
 )
 GO
 ALTER TABLE [dbo].[HelloDemo] ADD CONSTRAINT [PK__HelloDem__977B8075C276CD8C] PRIMARY KEY CLUSTERED  ([DemoId])
